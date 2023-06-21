8687a1a6ff0bde5be25f4ca7c6766257db4f218c
Dhruvi Kotecha <dhruvikotecha2001@gmail.com>
2023-06-21 15:41:42 +0530

    initial commit fastapi setup

--- fastapi/main.py
+++ fastapi/main.py
@@ -2,6 +2,33 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+users = {
+    1: {
+        "id": 1,
+        "name": "John Doe",
+        "city": "Ahmedabad",
+        "email": "john.doe@mailinator.com"
+    },
+    2: {
+        "id": 2,
+        "name": "John Doe",
+        "city": "Gandhinagar",
+        "email": "john.doe@mailinator.com"
+    },
+    3: {
+        "id": 3,
+        "name": "John Doe",
+        "city": "Rajkot",
+        "email": "john.doe@mailinator.com"
+    },
+    4: {
+        "id": 4,
+        "name": "John Doe",
+        "city": "Baroda",
+        "email": "john.doe@mailinator.com"
+    }
+}
+
 @app.get("/hello") # http://localhost:8000/hello GET
 def index():
     return { "message": "Hello World" }
@@ -12,3 +39,7 @@ def test():
     b = 20
     c = a + b
     return { "message": "Test API", "total": c }
+
+@app.get("/users")
+def get_users():
+    return {"message": "Users List", "data": users}
