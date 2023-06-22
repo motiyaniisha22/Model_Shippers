246845c766a9bc394d38ca53b3f437a80e9d5eea
Dhruvi Kotecha <dhruvikotecha2001@gmail.com>
2023-06-22 14:22:43 +0530

    CRUD 22/June

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -29,6 +30,17 @@ users = {
     }
 }
 
+class User(BaseModel):
+    id: int
+    name: str
+    city: str
+    email: str
+
+class UserUpdate(BaseModel):
+    name: str
+    city: str
+    email: str
+
 @app.get("/hello") # http://localhost:8000/hello GET
 def index():
     return { "message": "Hello World" }
@@ -42,8 +54,39 @@ def test():
 
 @app.get("/users")
 def get_users(city: str = None):
-    return { "message": "Users List", "data": users, "filter":city }
+    if city is None:
+        return { "message": "Users List", "data": list(users.values()) }
+    
+    filtered_users = [user for user in users.values() if user.get('city').lower() 
+    == city.lower()]
+    return { "message": "Users List", "data": filtered_users}
 
 @app.get("/users/{user_id}") # GET baseUrl/users/1
 def get_user_by_id(user_id: int, city: str):
     return { "message": "User detail", "data": users[user_id] } 
+
+@app.post("/users")
+def create_user(user: User):
+    users.update({user.id: user})
+    return { "message": "New user", "data": user }
+
+@app.put("/users/{user_id}")
+def update_user(user_id: int, user: UserUpdate):
+    if user_id not in users.keys():
+        return { "message": "Invalid User Id"}
+
+    updated_user = users.get(user_id)
+    updated_user.update({ "name": user.name })
+    updated_user.update({ "city": user.city })
+    updated_user.update({ "email": user.email })
+    users.update({ user_id: updated_user})
+
+    return {"message": "User details updated successfully", "data": updated_user }
+
+@app.delete("/users/{user_id}")
+def delete_user(user_id: int):
+    if user_id not in users.keys():
+        return{ "message": "Invalid User Id"}
+    
+    del users[user_id]
+    return {"message": "User deleted successfully", "data": users}
